e8ab8b8c5d1b4338fdf4c651b0b1a07842b29c37
Seyoon Ragavan <complexzetaprime@gmail.com>
2021-01-03 15:22:41 +1100

    Support for visualising weights by layer

--- visualise_weights.py
+++ visualise_weights.py
@@ -8,12 +8,14 @@ from Models.lottery_resnet import wide_resnet32
 
 # wgt_model: typically the model at init, this is the model where the weights come from
 # mask_model: the model from which we get the mask to decide which weights survived, if not provided assume all weights survive
-def visualise_surviving_weights(wgt_mdl, mask_mdl=None, rescale=False, bins=50):
+def visualise_surviving_weights(wgt_mdl, mask_mdl=None, rescale=False, bins=50, layerwise=False):
     wgt_sd = wgt_mdl.state_dict()
     if mask_mdl:
         mask_sd = mask_mdl.state_dict()
 
-    all_wgts = []
+    if not layerwise:
+        all_wgts = []
+
     for name, param in wgt_sd.items():
         # latter conditions remove unprunable parameters i.e. residual connections and batchnorm layers
         if name.endswith('weight') and 'shortcut' not in name and 'bn' not in name:
@@ -25,15 +27,23 @@ def visualise_surviving_weights(wgt_mdl, mask_mdl=None, rescale=False, bins=50):
             if mask_mdl:
                 flat_mask = mask_sd[name + '_mask'].reshape(-1).bool()
                 flat_wgt = flat_wgt[flat_mask]
-            all_wgts.append(copy.deepcopy(flat_wgt))
 
-    all_wgts = torch.cat(all_wgts)
-    print("Number of weights", all_wgts.shape)
-    plt.hist(all_wgts.detach().cpu().numpy(), bins=bins)
-    plt.show()
+            if layerwise:
+                print(name, flat_wgt.shape)
+                plt.hist(flat_wgt.detach().cpu().numpy(), bins=bins)
+                plt.show()
+                print("-----")
+            else:
+                all_wgts.append(copy.deepcopy(flat_wgt))
+
+    if not layerwise:
+        all_wgts = torch.cat(all_wgts)
+        print("Number of weights", all_wgts.shape)
+        plt.hist(all_wgts.detach().cpu().numpy(), bins=bins)
+        plt.show()
 
 # rescale: whether to standardise the weights in each layer before visualising
-def visualise(wgt_mdl=None, wgt_file=None, mask_mdl=None, mask_file=None, arch=None, rescale=False, bins=50):
+def visualise(wgt_mdl=None, wgt_file=None, mask_mdl=None, mask_file=None, arch=None, rescale=False, bins=50, layerwise=False):
     if arch == 'vgg16-bn':
         if not wgt_mdl:
             wgt_mdl = vgg16_bn((3, 32, 32), 10, False, False)
@@ -51,4 +61,4 @@ def visualise(wgt_mdl=None, wgt_file=None, mask_mdl=None, mask_file=None, arch=N
     else:
         raise(Exception("Invalid arch"))
 
-    visualise_surviving_weights(wgt_mdl, mask_mdl, rescale, bins)
+    visualise_surviving_weights(wgt_mdl, mask_mdl, rescale, bins, layerwise)
